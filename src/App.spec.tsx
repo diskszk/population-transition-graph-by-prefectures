9883e4f8f98d3545cd19baf20d2df35c7ced12c0
diskszk <ibalucky23@gmail.com>
2022-05-13 09:49:40 +0900

    feat: add pre-commit script

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App.tsx", () => {
+  test("Hello vite-react と表示される", () => {
+    render(<App />);
+    expect(screen.getByText("Hello vite-react")).toBeInTheDocument();
+  });
+});
