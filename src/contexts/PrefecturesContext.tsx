554f27698f9d4fa7df33c2402763f3918531422c
diskszk <49810133+diskszk@users.noreply.github.com>
2022-05-19 10:01:47 +0900

    feat: useContextをHome.tsxから分離 (#32)

---
+++ src/contexts/PrefecturesContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+import { Prefecture } from "../types";
+
+const prefecturesContext = createContext<Prefecture[]>([]);
+const setPrefecturesContext = createContext<
+  React.Dispatch<React.SetStateAction<Prefecture[]>>
+>(() => undefined);
+
+type Props = {
+  children: ReactNode;
+};
+
+export const PrefecturesProvider: React.FC<Props> = ({ children }) => {
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+
+  return (
+    <prefecturesContext.Provider value={prefectures}>
+      <setPrefecturesContext.Provider value={setPrefectures}>
+        {children}
+      </setPrefecturesContext.Provider>
+    </prefecturesContext.Provider>
+  );
+};
+
+export const usePrefecturesValue = () => useContext(prefecturesContext);
+export const usePrefecturesSetValue = () => useContext(setPrefecturesContext);
