c843c30ee3b776bb4f6dab706714e351a2915ca6
diskszk <ibalucky23@gmail.com>
2022-06-17 22:17:12 +0900

    fix: Checkboxコンポーネントのstorybookのビルドエラーを修正

---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentProps } from "react";
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { mockedHokkaido } from "../../mocks/mockResponses";
+import { Prefecture } from "../../types";
+import { StyledComponent as Component } from "./Checkbox";
+
+export default {
+  title: "Components/Checkbox",
+  component: Component,
+} as ComponentMeta<typeof Component>;
+
+type Props = ComponentProps<typeof Component>;
+
+export const 北海道 = () => {
+  const props: Props = {
+    checkboxId: "1",
+    handleChange: () => void 0,
+    prefecture: mockedHokkaido,
+  };
+
+  return <Component {...props} />;
+};
+
+// 別の書き方
+const okinawa: Prefecture = {
+  prefCode: 47,
+  prefName: "沖縄県",
+  populations: [],
+};
+
+export const 沖縄県: ComponentStoryObj<typeof Component> = {
+  args: {
+    checkboxId: "47",
+    handleChange: () => void 0,
+    prefecture: okinawa,
+  } as Props,
+};
