450d18b7df874eb08bb1684f5af1dbd97943f532
diskszk <49810133+diskszk@users.noreply.github.com>
2022-07-31 16:47:47 +0900

    fix: ライブラリのバージョンを最新にする (#66)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import env from "vite-plugin-env-compatible";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
